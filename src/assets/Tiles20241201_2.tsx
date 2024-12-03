46aec4f4d9d5d455ab19c281da396647bb548f31
aiyyskhan <aiyyskhan@gmail.com>
2024-12-03 15:31:28 +0900

    added maze_4.json

---
+++ src/assets/Tiles20241201_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Tiles20241201_2" tilewidth="16" tileheight="16" spacing="1" tilecount="24" columns="6">
+ <image source="Tiles20241201_2.png" width="101" height="67"/>
+</tileset>
